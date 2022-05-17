543d2682d4e8869a64221f8a9545f857473c4e9e
lucasrosenbusch <lucas.rosenbusch@stud.leuphana.de>
2022-05-17 17:24:44 +0200

    functions created
    geocoding and current aqi check are now in one function called current_aq

--- airquality/R/airquality.R
+++ airquality/R/airquality.R
@@ -1,64 +1,52 @@
+library(httr)
+library(jsonlite)
+
 #' @export
-
-
-# Shiny App ---------------------------------------------------------------
-library(shiny)
-
-ui <- fluidPage(
-
-  titlePanel("Air Quality"),
-
-  sidebarLayout(
-
-    sidebarPanel(
-
-      numericInput(inputId = "lat_in",
-                   label = "Enter Latitude",
-                   value = "",
-                   width = 400
-
-      ),
-
-      numericInput(inputId = "long_in",
-                   label = "Enter Longitude",
-                   value = "",
-                   width = 400
-
-      )
-    ),
-
-    mainPanel(
-      tableOutput(outputId = "aqi_out")
-    )
+current_aq <- function(city, country){
+  
+  #geocoding
+  geocode_base <- 'http://api.openweathermap.org/geo/1.0/direct?q='
+  geocode_limit <- '&limit=1&appid='
+  geocode_appid <- '03782ca206139ca19d564d33c2813127'
+  
+  geocode_url <- paste0(
+    geocode_base,
+    city,
+    ",",
+    country,
+    geocode_limit,
+    geocode_appid
   )
-)
+  
+  geocode_raw <- GET(geocode_url)
+  geocode_char <- rawToChar(geocode_raw$content)
+  geocode_dat <- fromJSON(geocode_char)
+  
+  geocode_dat[1 , c(3,4)]
 
-server <- function(input, output) {
-
-  output$aqi_out <- renderTable({
-
-    base <- 'http://api.openweathermap.org/data/2.5/'
-    which <- 'air_pollution?'
-    lat <- 'lat='
-    lon <- '&lon='
-    appid <- '&appid=03782ca206139ca19d564d33c2813127'
-
-    url <- paste0(base,
-                  which,
-                  lat,
-                  input$lat_in,
-                  lon,
-                  input$long_in,
-                  appid
-    )
-
-    dat <- GET(url)
-    dat_1 <- rawToChar(dat$content)
-    dat_2 <- fromJSON(dat_1)
-    dat_2$list[[1]]
-  })
-
+  #current airquality
+  base <- 'http://api.openweathermap.org/data/2.5/'
+  which <- 'air_pollution?'
+  lat <- 'lat='
+  lat_number <- geocode_dat[1,3]
+  lon <- '&lon='
+  lon_number <- geocode_dat[1,4]
+  appid <- '&appid=03782ca206139ca19d564d33c2813127'
+  
+  url <- paste0(base,
+                which,
+                lat,
+                lat_number,
+                lon,
+                lon_number,
+                appid
+  )
+  
+  dat <- GET(url)
+  dat_1 <- rawToChar(dat$content)
+  dat_2 <- fromJSON(dat_1)
+  dat_2$list[[1]]
 }
 
-# Create Shiny app ----
-shinyApp(ui = ui, server = server)
+
+
