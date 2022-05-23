c2067483de010c1abc28e7071c59be40fde0ca98
lucasrosenbusch <lucas.rosenbusch@stud.leuphana.de>
2022-05-23 19:59:20 +0200

    function structure + plots added

--- airquality/R/main.R
+++ airquality/R/main.R
@@ -5,23 +5,22 @@
 #' @import lubridate
 NULL
 
-#' Current Airquality Index
+#' Geocoding
 #'
-#' With this function you can get the current air quality data of any town.
+#' This function gives the coordinates of a given city. It used for further functions which require coordinates instead of city names.
 #'
 #' @param city City (in quotation marks)
 #' @param country Country Code (according to ISO 3166; e.g.: NL for Netherlands; in quotation marks)
 #'
-#' @return The airquality index (aqi) and the subcomponents (co, no, no2, o3, so2, pm2_5, pm10, nh3)
+#' @return Coordinates (latitude, longitude)
 #'
-#' @examples current_aq("Amsterdam", "NL")
+#' @examples geocoding("Amsterdam", "NL")
 #'
-#' @details The airquality index ranges from 1 = Good to 5 = Very Poor.
+#' @details It is important to enter the city and the country.
 #'
 #' @export
-current_aq <- function(city, country){
+geocoding <- function(city, country){
 
-  #geocoding
   geocode_base <- 'http://api.openweathermap.org/geo/1.0/direct?q='
   geocode_limit <- '&limit=1&appid='
   geocode_appid <- '03782ca206139ca19d564d33c2813127'
@@ -37,16 +36,35 @@ current_aq <- function(city, country){
 
   geocode_raw <- GET(geocode_url)
   geocode_char <- rawToChar(geocode_raw$content)
-  geocode_dat <- fromJSON(geocode_char)
+  return(geocode_dat <- fromJSON(geocode_char))
 
+}
+
+#' Current Airquality Data
+#'
+#' With this function you can get the current air quality data of any town.
+#'
+#' @param city City (in quotation marks)
+#' @param country Country Code (according to ISO 3166; e.g.: NL for Netherlands; in quotation marks)
+#'
+#' @return The airquality index (aqi) and the subcomponents (co, no, no2, o3, so2, pm2_5, pm10, nh3) in a list object.
+#'
+#' @examples current_aq_list("Amsterdam", "NL")
+#'
+#' @details The airquality index ranges from 1 = Good to 5 = Very Poor.
+#'
+#' @export
+current_aq_list <- function(city, country){
+
+  coordinates <- geocoding(city, country)
 
   #current airquality
   base <- 'http://api.openweathermap.org/data/2.5/'
   which <- 'air_pollution?'
   lat <- 'lat='
-  lat_number <- geocode_dat[1,3]
+  lat_number <- coordinates[1,3]
   lon <- '&lon='
-  lon_number <- geocode_dat[1,4]
+  lon_number <- coordinates[1,4]
   appid <- '&appid=03782ca206139ca19d564d33c2813127'
 
   url <- paste0(base,
@@ -62,52 +80,26 @@ current_aq <- function(city, country){
   current_aq_char <- rawToChar(current_aq_raw$content)
   current_aq <- fromJSON(current_aq_char)
 
-  #converting to data frame
-  current_aqi_df <- current_aq[['list']][['main']]
-  current_aq_comp_df <- current_aq[['list']][['components']]
+  return(current_aq)
+}
 
-  print(current_aqi_df)
-  print(current_aq_comp_df)
-
-  }
-
-
-#' co concentration of the last two weeks
+#' Two Week Airquality Data
 #'
-#' This function plots the co concentration of the last two weeks of any town.
+#' With this function you can get the air quality data of any town for the last two weeks.
 #'
 #' @param city City (in quotation marks)
 #' @param country Country Code (according to ISO 3166; e.g.: NL for Netherlands; in quotation marks)
 #'
-#' @return Line graph of the co concentration of the last two weeks.
+#' @return The airquality index (aqi), the subcomponents (co, no, no2, o3, so2, pm2_5, pm10, nh3), and the time stamps in a list object.
 #'
-#' @examples history_aq("Amsterdam", "NL")
+#' @examples history_aq_list("Amsterdam", "NL")
 #'
-#' @details The x-axis does not display the correct time yet.
+#' @details The airquality index ranges from 1 = Good to 5 = Very Poor.
 #'
 #' @export
-history_aq <- function(city, country){
+history_aq_list <- function(city, country){
 
-    #geocoding
-  geocode_base <- 'http://api.openweathermap.org/geo/1.0/direct?q='
-  geocode_limit <- '&limit=1&appid='
-  geocode_appid <- '03782ca206139ca19d564d33c2813127'
-
-  geocode_url <- paste0(
-    geocode_base,
-    city,
-    ",",
-    country,
-    geocode_limit,
-    geocode_appid
-  )
-
-  geocode_raw <- GET(geocode_url)
-  geocode_char <- rawToChar(geocode_raw$content)
-  geocode_dat <- fromJSON(geocode_char)
-
-  lat_number <- geocode_dat[1,3]
-  lon_number <- geocode_dat[1,4]
+  coordinates <- geocoding(city, country)
 
   #get time stamps
   current_time <- Sys.time()
@@ -122,18 +114,16 @@ history_aq <- function(city, country){
 
   #get history aq data
   hist_aq_url_1 <- "http://api.openweathermap.org/data/2.5/air_pollution/history?lat="
-  lat_number
   hist_aq_url_2 <- "&lon="
-  lon_number
   hist_aq_url_3 <- "&start="
   hist_aq_url_4 <- "&end="
   hist_aq_url_5 <- "&appid="
   appid <- '03782ca206139ca19d564d33c2813127'
 
   hist_aq_url <- paste0(hist_aq_url_1,
-                        lat_number,
+                        coordinates[1,3],
                         hist_aq_url_2,
-                        lon_number,
+                        coordinates[1,4],
                         hist_aq_url_3,
                         two_weeks_back_unix_r,
                         hist_aq_url_4,
@@ -145,17 +135,67 @@ history_aq <- function(city, country){
   hist_aq_char <- rawToChar(hist_aq_raw$content)
   hist_aq_dat <- fromJSON(hist_aq_char)
 
-  #converting to data frame
-  hist_aqi_df <- hist_aq_dat[['list']][['main']]
-  hist_comp_df <- hist_aq_dat[['list']][['components']]
+  return(hist_aq_dat)
 
-  #plotting
-  hist_comp_df$time <- seq(1:nrow(hist_comp_df))
+}
+
+#' Airquality Subcomponents Plot (past two weeks)
+#'
+#' With this function you can plot each of the subcomponents for the airquality of the last two weeks.
+#'
+#' @param city City (in quotation marks)
+#' @param country Country Code (according to ISO 3166; e.g.: NL for Netherlands; in quotation marks)
+#' @param component The subcomponent you wish to plot
+#'
+#'
+#' @return A plot of the airquality subcomponent over the past two weeks.
+#'
+#' @examples plot_comp_hist("Amsterdam", "NL", "co")
+#'
+#' @details
+#'
+#' @export
+plot_comp_hist <- function(city, country, component){
+
+  data <- history_aq_list(city, country)
+  hist_comp_df <- data.frame(data[[2]][[2]], data[[2]][[3]])
+
+  hist_comp_df$time <- as_datetime(hist_comp_df$data..2....3..)
 
   hist_comp_df %>%
-    ggplot(aes(x = time, y = co)) +
+    ggplot(aes(x = time, y = .data[[component]])) +
+    geom_line()
+
+}
+
+#' Airquality Index Plot (past two weeks)
+#'
+#' With this function you can plot  the airquality index of the last two weeks.
+#'
+#' @param city City (in quotation marks)
+#' @param country Country Code (according to ISO 3166; e.g.: NL for Netherlands; in quotation marks)
+#'
+#'
+#' @return A plot of the airquality index over the past two weeks.
+#'
+#' @examples plot_comp_hist("Amsterdam", "NL")
+#'
+#' @details
+#'
+#' @export
+plot_aqi_hist <- function(city, country){
+
+  data <- history_aq_list(city, country)
+  hist_aqi_df <- data.frame(data[[2]][[1]], data[[2]][[3]])
+
+  hist_aqi_df$time <- as_datetime(hist_aqi_df$data..2....3..)
+
+
+  hist_aqi_df %>%
+    ggplot(aes(x = time, y = aqi)) +
     geom_line()
 }
+
 
 
 
