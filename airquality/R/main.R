44983684dd691e9b14ef7ab17a4e7e76fee513af
lucasrosenbusch <lucas.rosenbusch@stud.leuphana.de>
2022-05-25 14:43:06 +0200

    updates to the plot_aqi_hist function

--- airquality/R/main.R
+++ airquality/R/main.R
@@ -197,29 +197,47 @@ plot_comp_hist <- function(city = "Amsterdam", country = "NL", component = "co")
 #' @details
 #'
 #' @export
-plot_aqi_hist <- function(city = "Amsterdam", country = "NL"){
+plot_aqi_hist <- function(city = "Amsterdam", country = "NL") {
 
   data <- history_aq_list(city, country)
   hist_aqi_df <- data.frame(data[[2]][[1]], data[[2]][[3]])
 
+  #converting time
   hist_aqi_df$time <- as_datetime(hist_aqi_df$data..2....3..)
+  hist_aqi_df$time <- as.Date(hist_aqi_df$time)
 
 
-  aqi_hist_plot <- hist_aqi_df %>%
-    ggplot(aes(x = time, y = aqi)) +
-    geom_line(color = "deepskyblue3") +
-    scale_x_datetime(labels = scales::date_format("%Y-%m-%d "),
-                     date_breaks = "24 hours") +
-    ylim(1,5) +
+  #averaging data to day level and adding color column
+  hist_aqi_df <- hist_aqi_df %>%
+    mutate(time = floor_date(time, unit = "day")) %>%
+    group_by(time) %>%
+    summarize(aqi = mean(aqi)) %>%
+    mutate(aqi = round(aqi,0))
+
+  #plotting
+  hist_aqi_df %>%
+    ggplot(aes(x = time, y = aqi, fill = factor(aqi))) +
+    geom_bar(stat = "identity") +
+    scale_fill_manual(values = c("1" = "chartreuse2",
+                                 "2" = "gold",
+                                 "3" =" orange",
+                                 "4" = "orangered1",
+                                 "5" = "red4"),
+                      labels = c("1 | Good", "2 | Fair", "3 | Moderate", "4 | Poor", "5 | Very Poor")) +
+    labs(fill = "Airquality Index") +
+    scale_x_date(labels = scales::date_format("%Y-%m-%d"),
+                 date_breaks = "1 day",
+                 expand = c(0, 0)) +
+
     ggtitle(paste0("Airquality Index of ", city ," over the past two weeks")) +
     xlab("Time") +
     ylab("Airquality Index") +
+    ylim(c(0, 5)) +
 
     theme_classic() +
     theme(axis.text.x=element_text(angle=60, hjust=1),
           plot.title = element_text(hjust = 0.5))
 
-  ggplotly(aqi_hist_plot)
 }
 
 
