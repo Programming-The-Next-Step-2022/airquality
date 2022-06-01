4c5f7f926151d2ea68d89bbedf6468a7d3820990
lucasrosenbusch <lucas.rosenbusch@stud.leuphana.de>
2022-06-01 10:55:03 +0200

    rshiny updates

---
+++ airquality/R/airquality_shiny.R
@@ -0,0 +1,109 @@
+#' @import shiny
+NULL
+
+#' @export
+airquality_app <- function(){
+
+  ui <- fluidPage(
+
+  tabsetPanel(
+
+
+    tabPanel("Location",
+             textInput(inputId = "city_in",
+                       label = "City",
+                       width = 200
+             ),
+             textInput(inputId = "country_in",
+                       label = "Country",
+                       width = 200
+             )
+    ),
+
+    tabPanel("Current AQ",
+             gt_output(outputId = "current_aq_df")
+    ),
+
+    tabPanel("AQI History",
+             plotOutput(outputId = "plot_aqi_hist",
+                        width = 1200
+             )
+    ),
+
+    tabPanel("AQ Subcomponent History",
+             selectInput(inputId = "comp_in",
+                         label = "Subcomponent",
+                         choices = c("co", "no", "no2", "o3",
+                                     "so2", "pm2_5", "pm10", "nh3" ),
+                         width = 200),
+
+             plotlyOutput(outputId = "plot_comp_hist",
+                          width = 1200)
+    ),
+
+    tabPanel("Current AQI Europe",
+             gt_output(outputId = "current_aq_table")
+             ),
+
+    tabPanel("Current Weather",
+             gt_output(outputId = "current_weather")
+             ),
+
+    tabPanel("AQI Map",
+             plotOutput(outputId = "aqi_map",
+                        width = "100%",
+                        height = "800"
+                        )
+             )
+
+
+  )
+
+)
+
+
+server <- function(input, output) {
+
+  output$current_aq_df <- render_gt({
+
+    gt(current_aq_df(input$city_in, input$country_in))
+
+  })
+
+  output$plot_aqi_hist <- renderPlot({
+
+    plot_aqi_hist(input$city_in, input$country_in)
+
+  })
+
+  output$plot_comp_hist <- renderPlotly({
+
+    plot_comp_hist(input$city_in, input$country_in, input$comp_in)
+
+  })
+
+  output$current_aq_table <- render_gt({
+
+    current_aq_table()
+
+  })
+
+  output$current_weather <- render_gt({
+
+    current_weather(input$city_in, input$country_in)
+
+  })
+
+  output$aqi_map <- renderPlot({
+
+    aqi_map()
+
+  })
+
+
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
+
+}
